6f18f1142fb334c1d372788b98ca65f20667d210
sarochovalucie <sarl03@vse.cz>
2025-05-23 10:23:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Funkce pro načtení souboru wishlist.md přes fetch API
 fetch('wishlist.md')
-  .then(response => {
-    if (!response.ok) throw new Error('Nepodařilo se načíst markdown');
-    return response.text();
+  .then(res => {
+    if (!res.ok) throw new Error(`Chyba při načítání MD: ${res.status}`);
+    return res.text();
   })
-  .then(mdText => {
-    // Převod Markdownu na HTML pomocí marked
-    const html = marked.parse(mdText);
-
-    // Vložení výsledného HTML do divu
+  .then(text => {
+    const html = marked.parse(text);
     document.getElementById('content').innerHTML = html;
   })
   .catch(err => {
-    document.getElementById('content').innerText = 'Chyba při načítání wishlistu: ' + err;
+    document.getElementById('content').innerHTML = `<p style="color: red;">Chyba při načítání wishlistu: ${err.message}</p>`;
+    console.error(err);
   });

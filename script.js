29bbdb49e3a74a2a9f36a164a22f0c7cc281d39b
sarochovalucie <sarl03@vse.cz>
2025-05-23 10:50:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wishlistContainer = document.getElementById('wishlist');
 
-const markdownUrl = 'https://cdn.jsdelivr.net/gh/4ME280-Webdesign-a-uzivatelska-rozhrani/sarochovalucie-wishlist/blob/main/wishlist.md';
+const markdownUrl = 'https://cdn.jsdelivr.net/gh/4ME280-Webdesign-a-uzivatelska-rozhrani/sarochovalucie-wishlist@main/wishlist.md';
 
 fetch(markdownUrl)
   .then(response => {
@@ -8,7 +8,6 @@ fetch(markdownUrl)
     return response.text();
   })
   .then(markdown => {
-    // Převod markdown na HTML pomocí marked.js
     wishlistContainer.innerHTML = marked.parse(markdown);
   })
   .catch(error => {
